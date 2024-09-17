7608dcb406f94f9e9feddc9df383473a620825ea
trbe22nl1 <bennyt_1995@hotmail.com>
2024-09-18 00:47:38 +0200

    test

--- tests/test_base.py
+++
@@ -1,9 +0,0 @@
-from BE.calculator_helper import CalculatorHelper
-import pytest
-
-class Basetest:
-    def setup_method(self, method):
-        self.calc = CalculatorHelper()
-    def teardown_method(self, method):
-        del self.calc
-        
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,6 +1,11 @@
-from test_base import Basetest
 from BE.calculator_helper import CalculatorHelper
 import pytest
+
+class Basetest:
+    def setup_method(self, method):
+        self.calc = CalculatorHelper()
+    def teardown_method(self, method):
+        del self.calc
 
 class TestCalculator(Basetest): 
     @pytest.mark.parametrize("a, b, expected", [
